ef8ec99ab9775362ec8209f341def29e35d03415
Mofusya <kai10.nakagawa@gmail.com>
2026-06-16 00:31:44 +0900

    Release_1.20.1-alpha0.6b-forge

---
+++ src/main/java/net/mofusya/ornatelib/config/JsonConfig.java
@@ -0,0 +1,59 @@
+package net.mofusya.ornatelib.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import net.minecraftforge.fml.loading.FMLPaths;
+import org.openjdk.nashorn.internal.scripts.JO;
+
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class JsonConfig {
+    private final Gson gson;
+    private final Path path;
+    private JsonObject config;
+
+    public JsonConfig(String fileName) {
+        this(fileName, new JsonObject());
+    }
+
+    public JsonConfig(String fileName, JsonObject config) {
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+        this.path = FMLPaths.CONFIGDIR.get().resolve(fileName + ".json");
+        this.config = config;
+    }
+
+    public JsonObject get(){
+        return this.config;
+    }
+
+    public void load() {
+        try {
+            if (!Files.exists(this.path)) {
+                this.save();
+                return;
+            }
+
+            try (Reader reader = Files.newBufferedReader(this.path)) {
+                this.config = this.gson.fromJson(reader, JsonObject.class);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void save() {
+        try {
+            Files.createDirectories(this.path.getParent());
+
+            try (Writer writer = Files.newBufferedWriter(this.path)) {
+                this.gson.toJson(this.config, writer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
